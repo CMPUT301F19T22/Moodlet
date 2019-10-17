9a2be7b6124ee85a91d048148441241b213a6911
Tamara Bojovic <32472572+bojotamara@users.noreply.github.com>
2019-10-17 12:44:54 -0600

    Setup Android project and Nav Bar (#47)
    * Setup android studio project with navbar
    
    * Added map fragment and reorganized/renamed fragments
    
    * Replaced navbar icons

---
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/MapFragment.java
@@ -0,0 +1,19 @@
+package com.cmput3owo1.moodlet.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.cmput3owo1.moodlet.R;
+
+public class MapFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
---
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/SearchFragment.java
@@ -0,0 +1,19 @@
+package com.cmput3owo1.moodlet.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.cmput3owo1.moodlet.R;
+
+public class SearchFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
