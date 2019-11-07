4a57e404b628f3e57ac2e6d2259824e8d9aa16f8
Henry Wee <wjxhenry@gmail.com>
2019-11-06 23:39:51 -0700

    MapFragment Javadocs (#56)
    * Adding Javadoc
    
    * Additional docs
    
    * Minor updates to README
    
    * Add description of MapFragment class
    
    * Fixing typos

--- Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/MapFragment.java
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/MapFragment.java
@@ -18,20 +18,32 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 
+/**
+ * A fragment that holds a Google API MapView that displays the location of the user's mood event
+ * history and optionally displays the location of the user's followers' most recent mood event.
+ */
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private static final String TAG = "Moodlet";
     private static final LatLng EDMONTON = new LatLng(53.5444,-113.4909);
 
-    MapView mapView;
-    GoogleMap map;
+    private MapView mapView;
+    private GoogleMap map;
 
+    /**
+     * This function is called to have the fragment instantiate its user interface view.
+     * @param inflater The LayoutInflater object that can be used to inflate any views in the fragment.
+     * @param container  If non-null, this is the parent view that the fragment's UI should be attached to. The fragment should not add the view itself, but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state as given here.
+     * @return Return the View for the fragment's UI, or null.
+     */
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
+        // Obtain the view to inflate
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
+        // MapView setup
         mapView = view.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
@@ -39,48 +51,73 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         return view;
     }
 
+    /**
+     * Forwarding the MapView's onStart() lifecycle method
+     */
     @Override
     public void onStart() {
         super.onStart();
         mapView.onStart();
     }
 
+    /**
+     * Forwarding the MapView's onResume() lifecycle method
+     */
     @Override
     public void onResume() {
         super.onResume();
         mapView.onResume();
     }
 
+    /**
+     * Forwarding the MapView's onPause() lifecycle method
+     */
     @Override
     public void onPause() {
         super.onPause();
         mapView.onPause();
     }
 
+    /**
+     * Forwarding the MapView's onStop() lifecycle method
+     */
     @Override
     public void onStop() {
         super.onStop();
         mapView.onStop();
     }
 
+    /**
+     * Forwarding the MapView's onDestroy() lifecycle method
+     */
     @Override
     public void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
     }
 
+    /**
+     * Forwarding the MapView's onSaveInstanceState() lifecycle method
+     */
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
 
+    /**
+     * Forwarding the MapView's onLowMemory() lifecycle method
+     */
     @Override
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
     }
 
+    /**
+     * Called when the map is ready to be used after calling getMapAsync().
+     * @param map A non-null instance of a GoogleMap
+     */
     @Override
     public void onMapReady(GoogleMap map) {
         this.map = map;
