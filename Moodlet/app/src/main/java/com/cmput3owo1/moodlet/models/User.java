d52876fc47e1fe6e6c732f229bf37b784a09ea08
bojotamara <tamarabojovic@hotmail.com>
2019-11-07 14:38:02 -0700

    Javadoc for FollowRequest, SocialSituation

--- Moodlet/app/src/main/java/com/cmput3owo1/moodlet/models/User.java
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/models/User.java
@@ -3,6 +3,9 @@ package com.cmput3owo1.moodlet.models;
 public class User {
     private String username;
     private String fullName;
+    private String email;
+
+    public User() {}
 
     public User(String username) {
         this.username = username;
@@ -12,11 +15,23 @@ public class User {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getFullName() {
         return fullName;
     }
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
