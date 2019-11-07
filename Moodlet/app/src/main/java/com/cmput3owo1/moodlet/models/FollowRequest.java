9e0b2890a3c9334d03ba2ad7dd96662bd320c84f
Tamara Bojovic <32472572+bojotamara@users.noreply.github.com>
2019-11-07 15:06:26 -0700

    More javadoc (#64)
    * Add Javadoc for MoodEventAssociation
    
    * Javadoc for FollowRequest, SocialSituation

--- Moodlet/app/src/main/java/com/cmput3owo1/moodlet/models/FollowRequest.java
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/models/FollowRequest.java
@@ -1,19 +1,58 @@
 package com.cmput3owo1.moodlet.models;
 
+/**
+ * A class that represents a follow request from one {@link User} to another
+ */
 public class FollowRequest {
-    private FollowableUser requestFrom;
-    private FollowableUser requestTo;
+    private User requestFrom;
+    private User requestTo;
 
-    public FollowRequest(FollowableUser requestFrom, FollowableUser requestTo) {
+    /**
+     * Default constructor for the FollowRequest
+     */
+    public FollowRequest() {
+        // For Firebase
+    }
+
+    /**
+     * Constructor for the FollowRequest that takes the users involved in the request
+     * @param requestFrom The user the request is from
+     * @param requestTo The user the request is sent to
+     */
+    public FollowRequest(User requestFrom, User requestTo) {
         this.requestFrom = requestFrom;
         this.requestTo = requestTo;
     }
 
-    public FollowableUser getRequestFrom() {
+    /**
+     * This gets the user that the request is from
+     * @return The user
+     */
+    public User getRequestFrom() {
         return requestFrom;
     }
 
-    public FollowableUser getRequestTo() {
+    /**
+     * This sets the user that the request is from
+     * @param requestFrom The user
+     */
+    public void setRequestFrom(User requestFrom) {
+        this.requestFrom = requestFrom;
+    }
+
+    /**
+     * This gets the user that the request is sent to
+     * @return The user
+     */
+    public User getRequestTo() {
         return requestTo;
+    }
+
+    /**
+     * This sets the user that the request is sent to
+     * @param requestTo The user
+     */
+    public void setRequestTo(User requestTo) {
+        this.requestTo = requestTo;
     }
 }
