04d3715389e016098ed78c999ed079d5483978c7
Darren Tran <dtran1@ualberta.ca>
2019-11-07 15:09:45 -0700

    Issue/us 03.01.01.01 user registration (#55)
    * added my name to Team.txt
    
    * user registration and login works
    
    * removed imports and changed colour to accent colour
    
    * added auto login
    
    * moved login and registration to UserServices file
    
    * changes implementation of callback functions to use interfaces in user service
    
    * added comments and moved all instances of auth/collectionReference to user service
    
    * resolved merge conflict

--- Moodlet/app/src/main/java/com/cmput3owo1/moodlet/activities/MainActivity.java
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/activities/MainActivity.java
@@ -27,6 +27,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
 
+    }
 }
