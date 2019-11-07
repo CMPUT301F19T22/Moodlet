f01a283b4b7d475fe92305e28e89cd16ee012a61
jameshahaha <jameshahaha@icloud.com>
2019-11-06 23:49:12 -0700

    i forget what i changed

--- Moodlet/app/src/main/java/com/cmput3owo1/moodlet/activities/MoodEditorActivity.java
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/activities/MoodEditorActivity.java
@@ -1,13 +1,17 @@
 package com.cmput3owo1.moodlet.activities;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.LightingColorFilter;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -25,38 +29,81 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MoodEditorActivity extends AppCompatActivity {
 
+    boolean editMode;
     Spinner moodSpinner;
     Spinner socialSpinner;
     ImageView bg;
-    TextView moodLabel;
-    TextView reasonLabel;
+    TextView moodDisplay;
+    TextView socialDisplay;
+    EditText reasonEdit;
     ArrayAdapter<EmotionalState> moodAdapter;
     ArrayAdapter<SocialSituation> socialAdapter;
+
+    Button addMood;
+    Button toggleEdit;
+    Button confirmEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor_add);
-
-
-
-
+        editMode = false;
         moodSpinner = findViewById(R.id.moodSelected);
         socialSpinner= findViewById(R.id.socialSelected);
-        moodLabel = findViewById(R.id.moodLabel);
-        reasonLabel = findViewById(R.id.reasonLabel);
+        moodDisplay = findViewById(R.id.moodDisplay);
+        socialDisplay = findViewById(R.id.socialDisplay);
+        reasonEdit = findViewById(R.id.reasonEdit);
         bg = findViewById(R.id.bg_vector);
+
+        SimpleDateFormat;
+        pattern;
+
+        //temp buttons
+        addMood = findViewById(R.id.add_mood);
+        toggleEdit = findViewById(R.id.toggle_edit);
+        confirmEdit = findViewById(R.id.confirm_edit);
+
+        Intent intent = getIntent();
+        if(intent.hasExtra("add")){
+            editMode = true;
+            addMode();
+        }
+        else if(intent.hasExtra("view")){
+            editMode = false;
+            viewMode();
+        }
+
+
+        toggleEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editMode = !editMode;
+                if(editMode){
+                    editMode();
+                }else{
+                    viewMode();
+                }
+            }
+        });
+
+    }
+
+    public void addMode(){
+        addMood.setVisibility(View.VISIBLE);
+        moodSpinner.setVisibility(View.VISIBLE);
+        socialSpinner.setVisibility(View.VISIBLE);
+        moodDisplay.setVisibility(View.INVISIBLE);
+        socialDisplay.setVisibility(View.INVISIBLE);
+        reasonEdit.setClickable(true);
+        reasonEdit.setFocusable(true);
 
         moodAdapter = new ArrayAdapter<EmotionalState>(this, R.layout.mood_spinner_style, EmotionalState.values());
         moodAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        moodSpinner.setAdapter(moodAdapter);
 
         socialAdapter = new ArrayAdapter<SocialSituation>(this, android.R.layout.simple_spinner_item, SocialSituation.values());
         socialAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-
-        moodSpinner.setAdapter(moodAdapter);
         socialSpinner.setAdapter(socialAdapter);
-
 
         moodSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -69,9 +116,54 @@ public class MoodEditorActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-                moodLabel.setText("Mood");
             }
         });
     }
+
+    public void editMode(){
+        toggleEdit.setVisibility(View.INVISIBLE);
+        confirmEdit.setVisibility(View.VISIBLE);
+        moodSpinner.setVisibility(View.VISIBLE);
+        socialSpinner.setVisibility(View.VISIBLE);
+        moodDisplay.setVisibility(View.INVISIBLE);
+        socialDisplay.setVisibility(View.INVISIBLE);
+        reasonEdit.setClickable(true);
+        reasonEdit.setFocusable(true);
+
+        moodAdapter = new ArrayAdapter<EmotionalState>(this, R.layout.mood_spinner_style, EmotionalState.values());
+        moodAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        moodSpinner.setAdapter(moodAdapter);
+
+        socialAdapter = new ArrayAdapter<SocialSituation>(this, android.R.layout.simple_spinner_item, SocialSituation.values());
+        socialAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        socialSpinner.setAdapter(socialAdapter);
+
+        moodSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                EmotionalState selected = EmotionalState.values()[i];
+                String displayName = selected.getDisplayName();
+                int color = selected.getColor();
+                bg.setColorFilter(color);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+    }
+
+    public void viewMode(){
+        toggleEdit.setVisibility(View.VISIBLE);
+        reasonEdit.setClickable(false);
+        reasonEdit.setFocusable(false);
+        bg.setColorFilter(Color.GRAY);
+        moodSpinner.setVisibility(View.INVISIBLE);
+        socialSpinner.setVisibility(View.INVISIBLE);
+        moodDisplay.setVisibility(View.VISIBLE);
+        socialDisplay.setVisibility(View.VISIBLE);
+    }
+
+
 
 }
--- Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/MoodHistoryFragment.java
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/MoodHistoryFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,6 +14,8 @@ import androidx.fragment.app.Fragment;
 import com.cmput3owo1.moodlet.R;
 import com.cmput3owo1.moodlet.activities.MoodEditorActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.w3c.dom.Text;
 
 public class MoodHistoryFragment extends Fragment {
 
@@ -22,15 +25,29 @@ public class MoodHistoryFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_mood_history, container, false);
         addMood = rootView.findViewById(R.id.add_fab);
+        TextView moodHistory = rootView.findViewById(R.id.text_history);
+
 
         addMood.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
                 //onAddMoodClick
                 Intent intent = new Intent(rootView.getContext(), MoodEditorActivity.class);
+                intent.putExtra("add", true);
                 startActivity(intent);
             }
         });
+
+        moodHistory.setOnClickListener(new TextView.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //onAddMoodClick
+                Intent intent = new Intent(rootView.getContext(), MoodEditorActivity.class);
+                intent.putExtra("view", true);
+                startActivity(intent);
+            }
+        });
+
 
         return rootView;
 
