1e8fb4cc88481c4ae5b4aa395fc8c736c0e2e7b1
toado <tan4@ualberta.ca>
2019-11-07 21:40:03 -0700

    Implement recyclerview

---
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/adapters/MoodEventAdapter.java
@@ -0,0 +1,73 @@
+package com.cmput3owo1.moodlet.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cmput3owo1.moodlet.R;
+import com.cmput3owo1.moodlet.models.MoodEvent;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MoodEventAdapter extends RecyclerView.Adapter<MoodEventAdapter.ViewHolder> {
+
+    private ArrayList<MoodEvent> moodEventList;
+    private OnItemClickListener listener;
+
+    public interface OnItemClickListener {
+        void onItemClick(int pos);
+    }
+
+    public MoodEventAdapter(ArrayList<MoodEvent> moodEventList, OnItemClickListener listener) {
+        this.moodEventList = moodEventList;
+        this.listener = listener;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tvMood, tvDateTime, tvTimeDiff;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            tvMood = itemView.findViewById(R.id.mood_textview);
+            tvDateTime = itemView.findViewById(R.id.date_time_textview);
+            tvTimeDiff = itemView.findViewById(R.id.time_difference);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            listener.onItemClick(position);
+        }
+    }
+
+    @NonNull
+    @Override
+    public MoodEventAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.moodhistory_row, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        MoodEvent mood = moodEventList.get(position);
+
+
+        // set TextViews in RecyclerView
+        holder.tvMood.setText(mood.getEmotionalState().name());
+        holder.tvDateTime.setText(new SimpleDateFormat("MMMM dd, yyyy - HH:mm").format(new Date()));
+        holder.tvTimeDiff.setText("1s");
+
+    }
+
+    @Override
+    public int getItemCount() { return moodEventList.size(); }
+}
--- Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/MoodHistoryFragment.java
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/MoodHistoryFragment.java
@@ -4,16 +4,55 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.cmput3owo1.moodlet.R;
+import com.cmput3owo1.moodlet.adapters.MoodEventAdapter;
+import com.cmput3owo1.moodlet.models.EmotionalState;
+import com.cmput3owo1.moodlet.models.MoodEvent;
 
-public class MoodHistoryFragment extends Fragment {
+import java.util.ArrayList;
+
+public class MoodHistoryFragment extends Fragment implements MoodEventAdapter.OnItemClickListener {
+
+    private RecyclerView recyclerView;
+    private MoodEventAdapter recyclerAdapter;
+    private ArrayList<MoodEvent> moodEventList;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mood_history, container, false);
+        View view = inflater.inflate(R.layout.fragment_mood_history, container, false);
+
+        recyclerView = view.findViewById(R.id.mood_event_rv);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+//        MoodEvent mood = new MoodEvent();
+//        mood.setEmotionalState(EmotionalState.ANGRY);
+//        moodEventList.add(mood);
+
+        recyclerAdapter = new MoodEventAdapter(moodEventList, this);
+        recyclerView.setAdapter(recyclerAdapter);
+
+
+        return view;
+    }
+
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+
+    @Override
+    public void onItemClick(int pos) {
+        // Implement for editing a mood event
     }
 }
