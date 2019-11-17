ce7f488b19556e402f677cd48789c299b1c30115
bojotamara <tamarabojovic@hotmail.com>
2019-11-17 13:19:09 -0700

    Added UI for search

--- Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/SearchFragment.java
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/SearchFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,9 +16,19 @@ import com.cmput3owo1.moodlet.R;
  * Placeholder for the user search. Not implemented.
  */
 public class SearchFragment extends Fragment {
+    private SearchView userSearchView;
+    private ListView userListView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+
+        userSearchView = rootView.findViewById(R.id.user_search_view);
+        userListView = rootView.findViewById(R.id.user_list_view);
+
+        userSearchView.setIconifiedByDefault(false);
+        userSearchView.setQueryHint(getResources().getString(R.string.search_hint));
+
+        return rootView;
     }
 }
