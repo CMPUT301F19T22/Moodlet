b5f14545061066507b1aea96ad177809c15ca59e
Ny Tran <tntran1@ualberta.ca>
2019-11-17 20:19:50 -0700

    Add delete mood feature

--- Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/MoodHistoryFragment.java
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/MoodHistoryFragment.java
@@ -1,6 +1,9 @@
 package com.cmput3owo1.moodlet.fragments;
 
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +11,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -96,5 +100,56 @@ public class MoodHistoryFragment extends Fragment
         moodEventList.addAll(newHistory);
         recyclerAdapter.notifyDataSetChanged();
     }
+
+    /**
+     * Utility class to add swipe support to recycler view for deleting a mood event
+     */
+    ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+        /**
+         * Called when ItemTouchHelper wants to move the dragged item from its old position to the new position
+         * @param recyclerView The RecyclerView to which ItemTouchHelper is attached to
+         * @param viewHolder The ViewHolder which is being dragged by the user
+         * @param target  The ViewHolder over which the currently active item is being dragged
+         * @return If this method returns true, ItemTouchHelper assumes viewHolder has been moved to the adapter position of target ViewHolder, else, it hasn't been moved
+         */
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        /**
+         * Called when a ViewHolder is swiped by the user
+         * @param viewHolder The ViewHolder which has been swiped by the user
+         * @param direction The direction to which the ViewHolder is swiped
+         */
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            moodEventList.remove(viewHolder.getAdapterPosition());
+            recyclerAdapter.notifyDataSetChanged();
+        }
+
+        /**
+         * Called by ItemTouchHelper on RecyclerView's onDraw callback
+         * @param c The canvas which RecyclerView is drawing its children
+         * @param recyclerView The RecyclerView to which ItemTouchHelper is attached to
+         * @param viewHolder The ViewHolder which is being interacted by the User (or it was interacted and simply animating to its original position)
+         * @param dX Amount of horizontal displacement caused by user's action
+         * @param dY Amount of vertical displacement caused by user's action
+         * @param actionState The type of interaction on the View
+         * @param isCurrentlyActive  True if view is currently being controlled by the user or false it is animating back to its original state
+         */
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+            View itemView = viewHolder.itemView;
+
+            final ColorDrawable background = new ColorDrawable(Color.RED);
+            background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(),   itemView.getRight(), itemView.getBottom());
+            background.draw(c);
+
+        }
+    };
 }
 
