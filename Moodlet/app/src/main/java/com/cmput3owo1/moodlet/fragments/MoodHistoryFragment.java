4792f1640a914dc37476c908f19ead95e3d5e15c
Ny Tran <tntran1@ualberta.ca>
2019-11-17 20:23:10 -0700

    Add delete feature

--- Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/MoodHistoryFragment.java
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/MoodHistoryFragment.java
@@ -56,6 +56,8 @@ public class MoodHistoryFragment extends Fragment
 
         moodEventList = new ArrayList<>();
 
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView);
+
         recyclerAdapter = new MoodEventAdapter(moodEventList, this);
         recyclerView.setAdapter(recyclerAdapter);
 
