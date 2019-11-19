cf678e9e99ae33592e7c5e97d3c3234edf503e94
bojotamara <tamarabojovic@hotmail.com>
2019-11-18 20:59:00 -0700

    Fix uncentered searchview

--- Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/SearchFragment.java
+++ Moodlet/app/src/main/java/com/cmput3owo1/moodlet/fragments/SearchFragment.java
@@ -5,9 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import com.cmput3owo1.moodlet.R;
@@ -41,7 +41,6 @@ public class SearchFragment extends Fragment implements
         userAdapter = new UserListAdapter(getContext(), userDataList, this);
         userListView.setAdapter(userAdapter);
 
-        userSearchView.setIconifiedByDefault(false);
         userSearchView.setOnQueryTextListener(this);
 
         return rootView;
