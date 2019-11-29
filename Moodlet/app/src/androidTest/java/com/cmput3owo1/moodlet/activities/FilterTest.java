e9473c54fdc17fb9d5d3b72a1d45fb83fd167603
toado <tan4@ualberta.ca>
2019-11-29 14:01:56 -0700

    Removed auto logout

--- Moodlet/app/src/androidTest/java/com/cmput3owo1/moodlet/activities/FilterTest.java
+++ Moodlet/app/src/androidTest/java/com/cmput3owo1/moodlet/activities/FilterTest.java
@@ -3,18 +3,15 @@ package com.cmput3owo1.moodlet.activities;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import com.cmput3owo1.moodlet.R;
-import com.cmput3owo1.moodlet.activities.LoginActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
--- Moodlet/app/src/androidTest/java/com/cmput3owo1/moodlet/activities/FollowerRequestTest.java
+++ Moodlet/app/src/androidTest/java/com/cmput3owo1/moodlet/activities/FollowerRequestTest.java
@@ -8,11 +8,9 @@ import androidx.test.espresso.ViewInteraction;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import com.cmput3owo1.moodlet.R;
-import com.cmput3owo1.moodlet.activities.LoginActivity;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -22,7 +20,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
